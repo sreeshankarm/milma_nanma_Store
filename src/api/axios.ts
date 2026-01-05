cac3a9d133249ceb4bc68fff249f76a190b2dec2
sreeshankarm <sreeshankar.m@duk.ac.in>
2026-01-05 11:59:51 +0530

    vercel_fix_error

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -4,6 +4,7 @@ import { token } from "../utils/token";
 const api = axios.create({
 //   baseURL: "https://nanmastagingapi.milma.in",
   baseURL: "/api", 
+    // baseURL: import.meta.env.VITE_API_BASE_URL,
 
 
   headers: {
